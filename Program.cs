69d3952bae5af537ecedfc94ef9113ab4414b085
Nora Mohamed <noramohamedfcis@gmail.com>
2019-12-09 14:17:31 +0200

    Initial commit of project template

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees
+{
+    struct Employee
+    {
+        int emp_ID, emp_securityLevel;
+        float emp_salary;
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
